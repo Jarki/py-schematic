d514e3eec2d05fefd61bc9cc0d148ff3111544d7
Jarki <yaroslav.ivashko@gmail.com>
2025-11-28 22:15:01 +0200

    feat: add ruff for linting/formatting + config; feat: add pytest for testing + cofig; refactor: rename project_slug -> package_name

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -9,7 +9,11 @@ echo "Running uv sync..."
 uv sync
 echo "Installing dependencies..."
 uv add loguru
+uv add python-dotenv
 echo "Adding development dependencies..."
 uv add --group dev mypy
+uv add --group dev ruff
+echo "Adding testing dependencies..."
+uv add --group test pytest
 
 echo "Setup complete. Your venv is ready at ./.venv"
