d13105386d28f16e7083a9b85a272e2b9f721002
jarki <yaroslav.ivashko@gmail.com>
2026-04-08 12:23:36 +0300

    feat: basic fastapi + htmx config

--- {{cookiecutter.package_name}}/src/settings.py
+++ {{cookiecutter.package_name}}/src/settings.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     log_level: str = "INFO"
+    host: str = "127.0.0.1"
+    port: int = 8000
 
     model_config = SettingsConfigDict(
         env_file=".env",
